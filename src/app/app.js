c5936838cbdd09f6f7ae1e197721ea48ea16d952
Nikx Fabrizio <w3c.nadeem@hotmail.com>
2017-11-08 03:06:56 +0530

    1

--- src/app/app.js
+++ src/app/app.js
@@ -39,7 +39,7 @@ Promise.resolve()
 				"hostname": "api.themoviedb.org",
 				"port": null,
 				"path": "/3/movie/"+item.id+"?language=en-US&api_key=56fd94ed1618bd7235d227829acdfaa1",
-				"headers": {} 
+				"headers": {}
 			};
 	  	let req = http.request(options, function (res) {
 		  let chunks = [];
@@ -90,106 +90,3 @@ Promise.resolve()
 	.then(function(){
 	  console.log('Bot stopped.');
 	});
-// tmdb.getMovies().then((movies) => {
-
-// 	tmdb.buffer = {
-// 		type: 'movies',
-// 		data: movies
-// 	};
-	
-// 	tmdb.buffer.data.forEach(function(movie, index){
-// 		console.log(movie);
-// 	});
-=======
-let util = require('./util');
-
-
-let fetchFromRemote = (id)=>{
-    console.log('fetching from remote for id: ', id);	
-    return new Promise((resolve, rejected)=>{
-    	setTimeout(()=>{
-	    	console.log("fetched!");	
-	    	resolve();
-	    },1000);
-    });
-}
-
-let prepareIdBuffer = (type)=>{
-	console.log('preparing id buffer for type: ', type);
-	return new Promise((resolve, reject)=>{
-		setTimeout(()=>{
-	    	console.log("prepared!");	
-	    	resolve([1,2,3,4,5,6,7,8,9,10]);
-	    },1000);
-	});
-}
-let save = ()=>{
-	console.log('saving...');
-	return new Promise((resolve, reject)=>{
-		setTimeout(()=>{
-	    	console.log('saved!');	
-	    	resolve(true);
-	    },1000);
-	});
-};
-
-Promise.resolve(5)
-.then((response)=>{
-	console.log('initializing...');
-	return 5;
-}).then(function repeat(response){
-	if(response<=0)
-		return;
-	else
-		return repeat(response-1);
-
-}).catch((error)=>{
-    console.log("error", error);
-
-});
-// tmdb.getMovies().then(function fetchMovies(movies){
-	
-
-// 	console.log(movies.length||0, 'movie ids fetched from db.');
-
-// 	tmdb.setBuffer('movies', movies);
-	
-// 	let idBuffer = tmdb.getBuffer();
-// 	let dataBuffer = tmdb.getBuffer();
-
-// //	if(!!!buffer)
-
-// 	// idBuffer.getData().forEach(function(movie, index){
-// 	// 	let id = util.getObjectId(movie);
-// 	// 	console.log('id', id);
-// 	// });
-
-// 	let step_1 = Promise.resolve(3)
-// 	.then((response) => {
-// 		console.log('Starting...');
-// 		return 3;
-// 	})
-// 	.then(function prepareIdBuffer(response){
-// 		console.log('preparing ID buffer...');
-// 		if(response<=0){
-// 			console.log('exiting from buffer.');
-// 			return;
-// 		}
-
-// 		console.log("response", response);
-// 		return prepareIdBuffer(response-1);
-
-// 	}).then((response)=>{
-// 		console.log('Fininshed.');
-// 	});
-// 	return step_1;
-
-
-
-
-// }).then((response)=>{
-// 	console.log('Finished.');
->>>>>>> 26c26337108100558623149995f1dd7948121c3f
-// }).catch((error) => {
-// 	console.log(error);
-// });
